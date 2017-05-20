0a04068f9572c8292040565fd73955f20d1f29f5
Kriss3 <kris_s2@outlook.com>
2017-05-20 01:13:11 -0700

    Add project files.

---
+++ COMP2614Midterm/MyApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP2614Midterm
+{
+	class MyApp
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
